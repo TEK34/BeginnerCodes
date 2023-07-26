b623df10092a3807071de82fe376d75a987232af
Alin Cosmin Lupu <94184491+TEK34@users.noreply.github.com>
2023-07-26 14:11:30 +0300

    Add files via upload

---
+++ ReadingValuesFromConsole.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout<< "Enter a value for x and y: ";
+double x;
+double y;
+// Cin is  the  command  to use  in  order to write the value of x & y
+// directly in the console instead of declaring them in separate variables.
+cin >> x>>y;
+cout<< x+y;
+return 0;
+}
+
