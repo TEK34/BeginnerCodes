5280cea2edcd0270a19b008b5e20daae5a48af64
Alin Cosmin Lupu <94184491+TEK34@users.noreply.github.com>
2023-07-26 03:22:59 +0300

    Add files via upload

---
+++ NamingConventions.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int file_size; //Snake Case
+    int FileSize;  //Pascal Case (Better for Classes)
+    int fileSize;  //Camel Case  (Better for Variables)
+    int iFileSize; //HungarianNotation
+return 0;
+}
