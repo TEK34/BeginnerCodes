f36af2b8f98433ac54791c47862495c8140d3f23
Alin Cosmin Lupu <94184491+TEK34@users.noreply.github.com>
2023-07-26 13:39:03 +0300

    Add files via upload

---
+++ Exercise1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//                       x+10
+//Exercise 1 : Implement ----
+//                        3y
+using namespace std;
+int main()
+{
+double x=10;
+int y=5;
+double z=(x+10)/(3*y);
+cout <<z;
+}
+
