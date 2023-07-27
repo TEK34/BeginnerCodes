6bb7daccd0b6d2fb24a225ef768e53be78188a99
Alin Cosmin Lupu <94184491+TEK34@users.noreply.github.com>
2023-07-27 21:12:07 +0300

    Add files via upload

---
+++ DataTypes.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+//Calculate the area of a circle using a radius written by the user.
+int main()
+{
+    //Respecting the sufix for float and long we can replace every data type with auto;
+double price=99.99;
+float interestRate=3.67f;
+long fileSize=90000L;
+char letter='a';
+bool isValid=false;
+/*
+*auto price=99.99;
+*auto interestRate=3.67f;
+*auto fileSize=90000L;
+*auto letter='a';
+*auto isValid=false;
+*return 0;
+*/
+}
